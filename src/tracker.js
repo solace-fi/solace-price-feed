88adfc0ca4bc88a7d04d2eeb9d2a08f38166d308
Andrew Leonard <leonardishere@gmail.com>
2022-06-06 15:57:38 -0700

    track bond prices

---
+++ src/tracker.js
@@ -0,0 +1,35 @@
+// tracks stats over time
+
+const { snsPublishError, s3GetObjectPromise } = require("./utils/utils")
+const { trackBondPrices } = require("./bondPrices")
+
+// Define headers
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE"
+}
+
+async function track() {
+  var res = await Promise.all([
+    trackBondPrices(),
+  ])
+  return res
+}
+
+// Lambda handler
+exports.handler = async function(event) {
+  try {
+    await track()
+    return {
+      statusCode: 200,
+      headers: headers
+    }
+  } catch (e) {
+    await snsPublishError(event, e)
+    return {
+      statusCode: 500,
+      headers: headers
+    }
+  }
+}
